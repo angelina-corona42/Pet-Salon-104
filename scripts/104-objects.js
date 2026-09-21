584912809565167baf680ac0085ff7d37816fece
Angelina Corona <angelina.corona92801@gmail.com>
2026-09-21 14:19:35 -0700

    added Java notes

--- scripts/104-objects.js
+++ scripts/104-objects.js
@@ -1,2 +1,46 @@
-// 1. Define the array 
+// Primitive / Basic 
+let name = "Angelina";
+let lastname = "Corna";
+let age = 25;
+let isStudent = false;
+
+let name1 = "Leticia";
+let lastname1 = "Cisneros";
+let age1 = "29";
+let isStudent1 = true; 
+
+//Non-Primative / Advance
+//OBJECT LITERAL
+// student1 = object , name = property 
+let student1 = {
+    name:"Angelina", 
+    lastname: "Corona", 
+    age: 25, 
+    isStudent: false
+};
+
+let student2 = {
+    name:"Ashlie", 
+    lastname: "Rodriguez", 
+    age: 28, 
+    isStudent: false
+};
+
+// Access to the values --> 
+// objectName.property
+// Student 1 name 
+console.log (student1.name);
+
+// Student 2 age
+console.log(student2.age);
+
+// Student 1 lastname
+console.log(student1.lastname);
+
+// Student 2 is Student?
+console.log(student2.isStudent);
+
+// INTERPILATION
+
+console.log(``);
 
